c5e3c6127f90b5d98bab19fd22cb24088d63c730
Diego <romero.diego31@gmail.com>
2020-10-12 17:31:44 +0200

    nettoyage code bike view model

--- app/src/main/java/com/example/geobike/viewmodel/BikeViewModel.java
+++ app/src/main/java/com/example/geobike/viewmodel/BikeViewModel.java
@@ -1,17 +1,13 @@
 package com.example.geobike.viewmodel;
 
 import android.app.Application;
-import android.content.Context;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-import com.example.geobike.httpRequest.BikeCalls;
 import com.example.geobike.models.Bike;
 import com.example.geobike.repositories.BikeRepository;
-import com.example.geobike.service.BikeService;
 
 import java.util.List;
 
@@ -19,13 +15,11 @@ import io.reactivex.rxjava3.core.Observable;
 
 public class BikeViewModel extends AndroidViewModel {
 
-    private LiveData<List<Bike>> bikes;
     private BikeRepository bikeRepository;
 
     public BikeViewModel(Application application){
         super(application);
         bikeRepository = new BikeRepository(application);
-        bikes = new MutableLiveData<>();
     }
 
     public Observable<List<Bike>> getAllBikeLiveData(){
